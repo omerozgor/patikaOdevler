d0ee5298be1d08be6b8962dbdd043e190ca0dbac
omerozgor <omerfarukozgor9@gmail.com>
2023-07-20 10:18:31 +0300

    Pratik Sınıfı Geçme yapıldı

---
+++ patikaJavaOdevler/pratikPassingLecture/src/Main.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double math, physics, chemistry, turkish, music,count=0, total=0.0, average;
+
+
+        System.out.print("Matematik notunuzu giriniz: ");
+        math = input.nextDouble();
+        if(math <= 100 && math >= 0){
+            total += math;
+            count++;
+        }
+
+        System.out.print("Fizik notunuzu giriniz: ");
+        physics = input.nextDouble();
+        if(physics <= 100 && physics >= 0){
+            total += physics;
+            count++;
+        }
+
+        System.out.print("Kimya notunuzu giriniz: ");
+        chemistry = input.nextDouble();
+        if(chemistry <= 100 && chemistry >= 0) {
+            total += chemistry;
+            count++;
+        }
+
+        System.out.print("Türkçe notunuzu giriniz: ");
+        turkish = input.nextDouble();
+        if(turkish <= 100 && turkish >= 0){
+            total += turkish;
+            count++;
+        }
+
+        System.out.print("Müzik notunuzu giriniz: ");
+        music = input.nextDouble();
+        if(music <= 100 && music >= 0){
+            total += music;
+            count++;
+        }
+
+        average = total / count;
+        System.out.println("Ortalamanız: " + average);
+
+        String message = average <= 55 ? "Sınıfta kaldınız!" : "Tebrikler sınıfı geçtiniz";
+        System.out.println(message);
+
+        input.close();
+    }
+}
