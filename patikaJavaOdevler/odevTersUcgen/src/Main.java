e47b75861a82871aa6304885b2148f00cc696e22
omerozgor <omerfarukozgor9@gmail.com>
2023-08-01 13:57:12 +0300

    Ödev Ters Üçgen yapıldı

---
+++ patikaJavaOdevler/odevTersUcgen/src/Main.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        try {
+            System.out.print("Bir sayı giriniz: ");
+            int n = input.nextInt();
+
+            for(int i=0; i<n; i++) {
+                for(int j=0; j<i; j++)
+                    System.out.print(" ");
+
+                for(int j=1; j<=(2*(n-i)-1); j++)
+                    System.out.print("*");
+
+                System.out.println();
+            }
+
+        } catch (Exception e) {
+            System.out.println("Hatali deger girdiniz");
+        } finally {
+            input.close();
+        }
+    }
+}
