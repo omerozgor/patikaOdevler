dc725bd81df5d0fa8bf567d3b926212568326159
omerozgor <omerfarukozgor9@gmail.com>
2023-08-01 14:16:13 +0300

    Pratik Öğrenci Bİlgi Sistemi yapıldı

---
+++ patikaJavaOdevler/pratikOgrenciBilgiSistemi/src/Course.java
@@ -0,0 +1,41 @@
+
+
+public class Course {
+    Teacher courseTeacher;
+    String name;
+    String code;
+    String prefix;
+    int note;
+    int verbalGrade;
+    double verbalPercent;
+
+    public Course(String name, String code, String prefix, int verbalPercent) {
+        this.name = name;
+        this.code = code;
+        this.prefix = prefix;
+        this.note = 0;
+        this.verbalGrade = 0;
+        this.verbalPercent = (double) verbalPercent / 100;
+    }
+
+    public void addTeacher(Teacher t) {
+        if (this.prefix.equals(t.branch)) {
+            this.courseTeacher = t;
+            System.out.println("��lem ba�ar�l�");
+        } else {
+            System.out.println(t.name + " Akademisyeni bu dersi veremez.");
+        }
+    }
+
+    public void printTeacher() {
+        if (courseTeacher != null) {
+            System.out.println(this.name + " dersinin Akademisyeni : " + courseTeacher.name);
+        } else {
+            System.out.println(this.name + " dersine Akademisyen atanmam��t�r.");
+        }
+    }
+
+    public void setVerbalPercent(int percent) {
+        this.verbalPercent = (double) percent / 100;
+    }
+}
