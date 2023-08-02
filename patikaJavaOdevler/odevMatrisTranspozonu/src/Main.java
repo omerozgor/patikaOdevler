580a96927e2c535431c9510e46fbcbbe42d8ef8a
omerozgor <omerfarukozgor9@gmail.com>
2023-08-02 13:37:17 +0300

    Ödev matris transpozonu yapıldı

--- patikaJavaOdevler/odevMatrisTranspozonu/src/Main.java
+++ patikaJavaOdevler/odevMatrisTranspozonu/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
         for(int i=0; i<matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
-                System.out.print(matrix[i][j] + " ");
+                System.out.print(matrix[i][j] + " ");//
             }
             System.out.println();
         }
